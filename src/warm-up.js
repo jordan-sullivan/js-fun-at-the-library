fa8fcfc5e1156ae9d433137f567632bedbd88545
Jordan Sullivan <helloandhi.jp@gmail.com>
2022-02-03 00:43:01 -0700

    Add completed warm up exercises

--- src/warm-up.js
+++ src/warm-up.js
@@ -10,79 +10,108 @@
 // Initialize two new variables, "title", and "pages", and assign values to them with
 // a book's title and page count.
 
-// var title = "Harry Potter";
-// var ...
-
+var title = "Harry Potter";
+var pages = 450;
 
 
 // Reassign (not initialize) your "pages" variable from above. An editor came around and
 // condensed the book, use a built in math operator to make the page count a quarter
-// of what it currently is 
-
+// of what it currently is
+pages = 450 / 4;
+console.log(pages)
+//112.5
 
 
 // Initialize a new variable "movie", but leave it undefined
-
+var movie =
 
 
 // Create a variable named "sequel", and assign it to a be a sequel of your
 // book (made up or not!). Your sequel must make use of your "title" variable from above
-
-
+var sequel = title + " ... And the Magic Stuff";
+console.log(sequel)
+//Harry Potter ...And the Magic Stuff
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the book you chose is your favorite book or not
 
-
-
+var isFavorite = true;
+console.log("Is " + sequel + " my favorite book? " + isFavorite)
+//Is Harry Potter ...And the Magic Stuff my favorite book? true
 
 // Create a new variable named "isShortStory", and use a comparison operator to
 // assign a boolean value to this variable. If "pages" is less than 25, the
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
 
-
+var isShortStory = (pages < 25);
+console.log(pages<25)
+//false
 
 //------------------------- Conditionals -------------------------//
 
 
 // Express the following in code: If the variable "pages" is less than 1000 log
-// the statement 'I could read that in a day!', otherwise, log the statement 
+// the statement 'I could read that in a day!', otherwise, log the statement
 // 'Ok, maybe I could read that in a day and a half'
-
-
+if (pages < 1000) {
+console.log("I could read that in a day!");
+} else {
+  console.log("Ok, maybe I could read that in a day and a half")
+}
+//I could read that in a day!
 
 // Express the following in code: If the variable "isFavorite" is true,
 // log the statement 'My favorite book is [title]!'. Otherwise, log the
 // statement 'I recently read the [title] book'. In either case, the value stored in
 // the "title" variable should be included in the logged statement.
-
-
+if (isFavorite == true) {
+  console.log("My favorite book is " + title + "!");
+} else {
+  console.log("I recently read the " + sequel + " book.")
+}
+//My favorite book is Harry Potter!
 
 //------------------------- Arrays -------------------------//
 
 
 // Create a new variable "books" and assign it to an Array of 5 different
 // book titles, represented by their names as Strings.
-
+var books = ["The Vanishing Half", "The Buddah and the Badass",
+"Okovango and the Gods", "Jack Reacher", "Beginner's Russian"];
+console.log(books)
+//[
+  //'The Vanishing Half',
+//  'The Buddah and the Badass',
+  //'Okovango and the Gods',
+//  'Jack Reacher',
+  //"Beginner's Russian"
+//]
 
 // Write the code to access the 3rd element of the Array, using bracket
 // notation.
-
+books[2]
+//Okovango and the Gods'
 
 // Create a new variable "belowTwoHundred" and assign it to an Array of 12
 // different numbers below two hundred.
-
+var belowTwoHundred = [1, 2, 3, 5, 66, 78, 67, 65, 34, 23, 12, 22]
+belowTwoHundred
 
 // Write the code to access the 5th element of the array, using bracket notation.
-
+belowTwoHundred[4]
 
 //------------------------- Objects -------------------------//
 
 // Create a new variable "dictionary" and assign to it an object literal with two
 // keys: name, and hasThesaurus. The value for name should the string 'Webster's', and
 // the value for hasThesaurus should be false
-
+var dictionary = {
+name: "Webster's",
+hasThesaurus: false,
+};
 
 // Write the code to add the key of pageCount, with the value of 720. Use dot
 // notation to do this. DO NOT modify your code from above.
+dictionary.pageCount = 720
+console.log(dictionary)
